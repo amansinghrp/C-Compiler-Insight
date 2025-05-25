25fc536c5a686493dbd4536232a718b1069a93db
Aman Singh <rajputaman6554@gmail.com>
2025-05-26 00:42:35 +0530

    Modified the grammar for semantic phase to handle printf function call

--- src/temp.c
+++ src/temp.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 int main(){
 	int a = 10;
-    int b;
-    scanf("%d", &b);
-    if(a < b){
-    	printf("b is large");
-    }
-    else{
-    	printf("a is large");
+    while(a > 1){
+        printf("%d\n", a);
+        a = a-2;
     }
     printf("End of program");
     return 0;
