3b9f11292849f20cdf76a589dbe1a2c2b658e919
Aman Singh <rajputaman6554@gmail.com>
2025-05-27 22:38:22 +0530

    Added auto scroll to output section feature

--- src/temp.c
+++ src/temp.c
@@ -5,19 +5,8 @@ int main() {
     int x;
     float f;
     int a=3;
-    a = x *  + 5;
-    if(x>a) {
-        printf("Hi!");
-        a = x * + 100;
-        if(x>a) {
-            printf("Hi!");
-            a = x * 3 + 100;
-        }
-        else {
-            x = a * 3 + 100;
-        }
+    while(a > 0){
+    	a = a-1;
     }
-    else {
-        x = a * 3 + 100;
-    }
+    return 0;
 }
